7ad334a0a9183268ba2cee4a3164fc6d8f80986f
jnosis <jnosis1546@gmail.com>
2024-03-07 02:08:12 +0900

    feat: Export BaseOptions and NotificationFields

--- mod.ts
+++ mod.ts
@@ -1,2 +1,7 @@
-export type { MessageOptions, MessageOptionsWithIds } from './types.ts';
+export type {
+  BaseOptions,
+  MessageOptions,
+  MessageOptionsWithIds,
+  NotificationFields,
+} from './types.ts';
 export { JoinJoaomgcd } from './joinjoaomgcd.ts';
