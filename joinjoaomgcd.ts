7694f4f4a750354504cd53d0728da0716a4dbb6d
jnosis <jnosis1546@gmail.com>
2024-03-07 19:24:11 +0900

    docs: Add JSDoc to joinjoaomgcd.ts

--- joinjoaomgcd.ts
+++ joinjoaomgcd.ts
@@ -15,6 +15,11 @@ export class JoinJoaomgcd {
     this.#apiKey = apiKey;
   }
 
+  /**
+   * Get your devices info list
+   *
+   * @returns The array of your devices infos
+   */
   async getDevices(): Promise<DeviceInfo[]> {
     const response = await fetch(
       `${BASE_URL}/registration/v1/listDevices?apikey=${this.#apiKey}`,
@@ -29,10 +34,22 @@ export class JoinJoaomgcd {
     throw new Error(devices.errorMessage || 'Unknown Error');
   }
 
+  /**
+   * Send message to a specific device or group
+   *
+   * @param options The options for send message with device id
+   * @returns The status of JoinJoaomgcd's response
+   */
   async sendMessage(options: MessageOptions): Promise<JoinStatus> {
     return await this.#sendMessage(options);
   }
 
+  /**
+   * Send message to specific devices
+   *
+   * @param options The options for send message with device ids
+   * @returns The status of JoinJoaomgcd's response
+   */
   async sendMessageWithIds(
     options: MessageOptionsWithIds,
   ): Promise<JoinStatus> {
